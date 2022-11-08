4941e955aaf39e4ff4533affbfcff65c26a7a11b
A2 <77829156+weizhengchu@users.noreply.github.com>
2022-11-08 19:16:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
+
+
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
@@ -50,4 +52,6 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ',add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
